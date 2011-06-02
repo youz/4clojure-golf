5a7154e543198fe06693970fcc308b542dfddb64
youz <citrus.yubeshi@gmail.com>
2011-06-02 22:32:14 +0900

    fix indent

--- 091_Graph Connectivity.clj
+++ 091_Graph Connectivity.clj
@@ -2,9 +2,9 @@
   (let [[h & r] (seq o)]
     ((fn f [c r]
        (or (e r)
-	 (let [n (mapcat #(filter (fn [p] (some (set %) p)) r) c)]
-	   (if (e n) false
-	     (f (reduce conj c n)
-		(remove (set n) r))))))
+	   (let [n (mapcat #(filter (fn [p] (some (set %) p)) r) c)]
+	     (if (e n) false
+	       (f (reduce conj c n)
+		  (remove (set n) r))))))
      #{h} r)))
 empty?
