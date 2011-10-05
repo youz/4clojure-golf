4ec068418e44d9253a0940b0c9e37d9e880da087
Yousuke Ushiki <citrus.yubeshi@gmail.com>
2011-10-05 17:53:22 +0900

    No124: omitted pruning

--- 124_Analyze Reversi.clj
+++ 124_Analyze Reversi.clj
@@ -4,12 +4,11 @@
         d (r 3)
         e #([(r % %2) (repeat %2 %) (r % -1 -1)] %3)]
     (a conj
-       (for [y (r H) x (r W) :when (= ((B y) x) 'e)]
+       (for [y (r H) x (r W)]
          (#({() {}} % {[y x] (set %)})
           (a concat
-             (for [u d v d :let [l (rest (map vector (e y H u) (e x W v)))]]
-               (if (= u v 1)
-                 ()
-                 (take (- (c (re-find ({'b #"^w+b"} P #"b+w")
-                                      (a str (map #(get-in B %) l))))
-                          1) l)))))))))
+             (for [u d v d :let [l (map vector (e y H u) (e x W v))]]
+               (rest
+                (take (- (c (re-find ({'b #"^ew+b"} P #"eb+w")
+                                    (a str (map #(get-in B %) l))))
+                         1) l)))))))))
