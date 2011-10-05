192550ba9ae445ddf8ac970dbb9896cb86c99962
Yousuke Ushiki <citrus.yubeshi@gmail.com>
2011-10-05 17:57:49 +0900

    No.124: specialized to 4x4 board

--- 124_Analyze Reversi.clj
+++ 124_Analyze Reversi.clj
@@ -1,13 +1,12 @@
 (fn [B P]
   (let [c count r range a apply
-        W (c B) H (c (B 0))
         d (r 3)
-        e #([(r % %2) (repeat %2 %) (r % -1 -1)] %3)]
+        e #([(r % 4) [% % % %] (r % -1 -1)] %2)]
     (a conj
-       (for [y (r H) x (r W)]
+       (for [y (r 4) x (r 4)]
          (#({() {}} % {[y x] (set %)})
           (a concat
-             (for [u d v d :let [l (map vector (e y H u) (e x W v))]]
+             (for [u d v d :let [l (map vector (e y u) (e x v))]]
                (rest
                 (take (- (c (re-find ({'b #"^ew+b"} P #"eb+w")
                                     (a str (map #(get-in B %) l))))
