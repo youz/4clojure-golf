517ba0676b4aba3c6374d4d368d5d260fa250bd4
youz <citrus.yubeshi@gmail.com>
2011-10-05 23:15:04 +0900

    No.124: 226 points

--- 124_Analyze Reversi.clj
+++ 124_Analyze Reversi.clj
@@ -1,13 +1,12 @@
 (fn [B P]
-  (let [c count r range a apply
-        d (r 3)
-        e #([(r % 4) [% % % %] (r % -1 -1)] %2)]
+  (let [r range a apply v vector
+        d #(v (r % 4) [% % % %] (r % -1 -1))]
     (a conj
        (for [y (r 4) x (r 4)]
          (#({() {}} % {[y x] (set %)})
           (a concat
-             (for [u d v d :let [l (map vector (e y u) (e x v))]]
+             (for [i (d y) j (d x) :let [l (map v i j)]]
                (rest
-                (take (- (c (re-find ({'b #"^ew+b"} P #"eb+w")
+                (take (- (count (re-find ({'b #"^ew+b"} P #"^eb+w")
                                     (a str (map #(get-in B %) l))))
                          1) l)))))))))
